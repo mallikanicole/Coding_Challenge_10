c28ca05ffe7d7d5c097e17c4bfb80cd711c036e9
mallikanicole <malmichael23@gmail.com>
2025-02-23 21:24:30 -0500

    update task 2

--- cc_10.js
+++ cc_10.js
@@ -30,6 +30,7 @@ class Order {
         this.orderID=orderID;
         this.product=product;
         this.quantity=quantity;
+        this.product.updateStock(quantity)
     };//create class with properties
     getOrderDetails(){
         return `Order ID:${this.orderID}, Product:${this.product.name}, Quantity:${this.quantity},Total Price:$${this.product.price*this.quantity}`
@@ -56,10 +57,27 @@ class Inventory {
     listProducts(){
         this.products.forEach(product=>console.log(product.getDetails()))
     };//log product details
+
+    placeOrder(orderID,product,quantity){
+        if(product.stock>=quantity){
+            let order=new Order(orderID,product,quantity)
+            this.orders.push(order)
+        };//add to orders
+    }
+    listOrders(){
+        this.orders.forEach(order=>console.log(order.getOrderDetails()))
+    };//log all placed orders
+
 }
+
 
 
 const inventory = new Inventory();
 inventory.addProduct(prod1);
 inventory.listProducts();
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5"
+inventory.placeOrder(601, prod1, 2);
+inventory.listOrders();
+// Expected output: "Order ID: 601, Product: Laptop, Quantity: 2, Total Price: $2400"
+console.log(prod1.getDetails());
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 3"
