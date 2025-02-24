9c4e3ae5c4cf8370e8e911448e1c4cec5c150dd7
mallikanicole <malmichael23@gmail.com>
2025-02-23 21:27:46 -0500

    Task 4 - Implemented Order Management

--- cc_10.js
+++ cc_10.js
@@ -57,6 +57,10 @@ class Inventory {
     listProducts(){
         this.products.forEach(product=>console.log(product.getDetails()))
     };//log product details
+   
+
+
+//Task 4 Implementing Order Management
 
     placeOrder(orderID,product,quantity){
         if(product.stock>=quantity){
@@ -71,11 +75,13 @@ class Inventory {
 }
 
 
-
+//test task 3
 const inventory = new Inventory();
 inventory.addProduct(prod1);
 inventory.listProducts();
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5"
+
+//test task 4
 inventory.placeOrder(601, prod1, 2);
 inventory.listOrders();
 // Expected output: "Order ID: 601, Product: Laptop, Quantity: 2, Total Price: $2400"
